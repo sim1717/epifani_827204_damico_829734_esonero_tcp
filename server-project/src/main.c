d307cd96b7267d297c8c9235d173d123a2f624c6
sim1717 <simonepifani05@gmail.com>
2025-11-25 11:32:18 +0100

    Update main.c

--- server-project/src/main.c
+++ server-project/src/main.c
@@ -12,8 +12,9 @@
 #endif
 
 #include <stdio.h>
+#include <ctype.h>
 #include "protocol.h"
-//////////////CLIENT
+///////////SERVER
 void clearwinsock() {
 #if defined WIN32
 	WSACleanup();
@@ -25,9 +26,7 @@ void errorhandler(char *errorMessage) {
 }
 
 int main(int argc, char *argv[]) {
-
-
-
+	srand(time(NULL));
 #if defined WIN32
 	// Initialize Winsock
 	WSADATA wsa_data;
@@ -38,106 +37,114 @@ int main(int argc, char *argv[]) {
 	}
 #endif
 
-	// create client socket
-	int c_socket;
-	c_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (c_socket < 0) {
+	// create welcome socket
+	int my_socket;
+	my_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (my_socket < 0) {
 		errorhandler("socket creation failed.\n");
-		closesocket(c_socket);
 		clearwinsock();
 		return -1;
 	}
 
-	// set connection settings
+	//set connection settings
 	struct sockaddr_in sad;
-	memset(&sad, 0, sizeof(sad));
+	memset(&sad, 0, sizeof(sad)); // ensures that extra bytes contain 0
 	sad.sin_family = AF_INET;
-	sad.sin_addr.s_addr = inet_addr("127.0.0.1"); // IP del server
-	sad.sin_port = htons(PROTO_PORT); // Server port
-
-	// connection
-	if (connect(c_socket, (struct sockaddr*) &sad, sizeof(sad)) < 0) {
-		errorhandler("Failed to connect.\n");
-		closesocket(c_socket);
+	sad.sin_addr.s_addr = inet_addr("127.0.0.1");
+	sad.sin_port = htons(PROTO_PORT); /* converts values between the host and network byte order. Specifically, htons() converts 16-bit quantities from host byte order to network byte order. */
+	if (bind(my_socket, (struct sockaddr*) &sad, sizeof(sad)) < 0) {
+		errorhandler("bind() failed.\n");
+		closesocket(my_socket);
 		clearwinsock();
 		return -1;
 	}
 
-	weather_request_t wr;
-	memset(&wr, 0, sizeof(wr));
-	int found_r = 0;
-
-	// Parsing corretto per gestire "p Reggio Calabria"
-	for (int i = 1; i < argc; i++) {
-	    if (strcmp(argv[i], "-r") == 0 && i + 1 < argc) {
-	    	found_r = 1;
-	        char *request_str = argv[++i];
-
-	        // Prendi il primo carattere come type
-	        wr.type=request_str[0];
-
-	        // Tutto il resto della stringa è la città
-	        // Salta il type e lo spazio (se presente)
-
-	        if (strlen(request_str) >= 2 && request_str[1] == ' ') {
-	            strcpy(wr.city, &request_str[2]);
-	        } else {
-	            // Se non c'è spazio, prendi tutto dopo il primo carattere
-	            strcpy(wr.city, &request_str[1]);
-	        }
-
-	    }
-	}
-	if (!found_r || strlen(wr.city) == 0) {
-	    printf("Usage: %s -r \"type city\"\n", argv[0]);
-	    printf("Esempio: %s -r \"t bari\"\n", argv[0]);
-	    return 0;
-	}
-	// send data to server
-	if (send(c_socket, (char *)&wr, sizeof(weather_request_t), 0) != sizeof(weather_request_t)) {
-		errorhandler("send() sent a different number of bytes than expected");
-		closesocket(c_socket);
+	// listen
+	if (listen(my_socket, QLEN) < 0) {
+		errorhandler("listen() failed.\n");
+		closesocket(my_socket);
 		clearwinsock();
 		return -1;
 	}
-	weather_response_t risposta;
 
-	// Ricevi la risposta dal server
-	if ((recv(c_socket, (char *)&risposta, sizeof(weather_response_t), 0)) <= 0) {
-	    errorhandler("recv() failed or connection closed prematurely");
-	    closesocket(c_socket);
-	    clearwinsock();
-	    return -1;
+	// accept new connection
+	struct sockaddr_in cad; // structure for the client address
+	int client_socket;       // socket descriptor for the client
+	int client_len;          // the size of the client address
+	printf("Waiting for a client to connect...\n\n");
+	while (1) {
+		client_len = sizeof(cad); // set the size of the client address
+		if ((client_socket = accept(my_socket, (struct sockaddr*) &cad,
+				&client_len)) < 0) {
+			errorhandler("accept() failed.\n");
+			// close connection
+			closesocket(client_socket);
+			clearwinsock();
+			return 0;
+		}
+
+
+
+
+		weather_request_t richiesta;
+		if ((recv(client_socket, (char *)&richiesta, sizeof(richiesta), 0)) <= 0) {
+					errorhandler("recv() failed or connection closed prematurely");
+					closesocket(client_socket);
+					clearwinsock();
+					return -1;
+				}
+
+		printf("Richiesta '%c %s' dal client ip %s\n", richiesta.type, richiesta.city, inet_ntoa(cad.sin_addr));
+		weather_response_t risposta;
+		char citylower[64];
+		strcpy(citylower, richiesta.city);
+		for(int i = 0; citylower[i]; i++) {
+		    citylower[i] = tolower(citylower[i]);
+		}
+
+		int city_found = 0;
+		for(int i = 0; i < NUM_CITIES; i++) {
+		    if(strcmp(citylower, SUPPORTED_CITIES[i]) == 0) {
+		        city_found = 1;
+		        break;
+		    }
+		}
+		if (city_found == 0){
+			risposta.status=1;
+			risposta.type = '\0';
+			risposta.value = 0.0;
+		}else{
+		switch (richiesta.type) {
+			case 't': risposta.type='t';
+					  risposta.status=0;
+					  risposta.value = get_temperature();
+				break;
+			case 'h': risposta.type='h';
+					  risposta.status=0;
+					  risposta.value = get_humidity();
+				break;
+			case 'w': risposta.type='w';
+					  risposta.status=0;
+					  risposta.value = get_wind();
+			break;
+			case 'p': risposta.type='p';
+				      risposta.status=0;
+				      risposta.value = get_pressure();
+				break;
+			default: risposta.status=2;
+					 risposta.type = '\0';
+			         risposta.value = 0.0;
+				break;
+		}
+		}
+		// send manipulated data to client
+		if (send(client_socket, (char *)&risposta, sizeof(risposta), 0) != sizeof(risposta)) {
+			errorhandler(
+					"send() sent a different number of bytes than expected");
+			closesocket(client_socket);
+			clearwinsock();
+			return -1;
+		}
 	}
-	printf("Ricevuto risultato dal server ip %s. ", inet_ntoa(sad.sin_addr));
 
-
-	switch(risposta.status){
-	case 0: printf("%s :",wr.city);
-			switch (risposta.type){
-			case 't': printf("Temperatura = ");
-					printf("%.1f°C", risposta.value);
-					break;
-			case 'h': printf("Umidità = ");
-			printf("%.1f%%", risposta.value);
-					break;
-			case 'w' : printf("Vento = ");
-			printf("%.1f km/h", risposta.value);
-					break;
-			case 'p':  printf("Pressione =");
-			printf("%.1f hPa", risposta.value);
-					break;
-			}
-
-	break;
-	case 1:printf("Città non disponibile");
-	break;
-	case 2:printf("Richiesta non valida");
-	break;
-	}
-
-	printf("\n");
-	closesocket(c_socket);
-	clearwinsock();
-	return 0;
 } // main end
